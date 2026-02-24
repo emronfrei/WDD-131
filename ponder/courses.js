8736165f1009fe0bc83cda797a34deb4c2e2cc3d
emronfrei <emronfrei@byui.edu>
2026-02-24 12:01:38 -0700

    Added courses.html

---
+++ ponder/courses.js
@@ -0,0 +1,15 @@
+
+const aCourse = {
+    code: 'CSE121b',
+    name: 'Javascript Language',
+    logo: 'images/web.png'
+  };
+console.log(aCourse.code);
+console.log(aCourse.name);
+
+document.querySelector('#courseName').textContent = aCourse.name;
+document.querySelector('#courseCode').textContent = aCourse.code;
+
+document.querySelector('img').setAttribute('src',aCourse.logo);
+document.querySelector('img').setAttribute('alt',aCourse.name);
+document.querySelector('img').style.width='100px';
